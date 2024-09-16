0067c7d226b5a7f9885117aa62167366ebce4721
I3WX <utkarshs7828@gmail.com>
2024-09-16 16:32:06 +0530

    .

--- dist/943e11c8fb4bcca74398.js
+++
@@ -1,6 +0,0 @@
-const addTodo = document.getElementById("add-todo-btn");
-const modal = document.getElementById("todo-modal");
-
-addTodo.addEventListener("click", () => {
-    modal.style.display = "block";
-});
